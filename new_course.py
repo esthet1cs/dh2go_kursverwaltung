34cad07437c0c26920cf75aefc29b4d09a45487c
cmcx <cms@dock.in-berlin.de>
2021-03-22 22:28:33 +0100

    initial

---
+++ new_course.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+# service script for building participant lists from a spreadsheet, suitable for usage in adduser and chpasswd
+# spreadsheet column used in this script: Name
+# expected input: csv-file named teiln.csv
+# output: 
+#   - user.list: a list of usernames
+#   - user.pass: a list in the format username:password, as expected by chpasswd
+
+import pandas as pd
+import string
+import secrets
+from unidecode import unidecode
+
+# helper functions
+
+def make_nk(name):
+    '''
+    input a name string, output a string with the first letter plus the last word
+    '''
+    name = name.lower()
+    name = name.split()
+    nk = name[0][0] + name[-1]
+    return nk
+
+def make_pw(length=8):
+    '''
+    return an alphanumerical password with the given length, defaults to 8 (taken from the secrets package tutorial)
+    '''
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    return password
+
+
+df = pd.read_csv('teiln.csv')                                   # read data from file
+
+
+### construct additional columns: usernames, passwords, user:pass
+
+df.Nutzungskennung = df.Name.apply(lambda x: make_nk(x))                # construct usernames column
+df.Nutzungskennung = df.Nutzungskennung.apply(lambda x: unidecode(x))    # change non-ascii-characters to ascii-compatible chars
+
+passwords = []                                                  # make a list with passwords matching the length of the user list
+for i in range(len(df)):
+    passwords.append(make_pw(8))
+    
+df["Passwort"] = passwords                                      # construct passwords columns = random password for each user
+df['userlist'] = df.Nutzungskennung + ':' + df.Passwort         # construct user:pass column for the user:pass file
+
+
+### export to file
+
+with open('user.list', 'w') as f:               # userlist
+    for user in df.Nutzungskennung:
+        f.write(user)
+        f.write('\n')
+        
+with open('user.pass', 'w') as f:           # user.pass
+    for userpass in df.userlist:
+        f.write(userpass + '\n')
+        
+df.to_csv('teiln_full.csv')                 # full participant csv file with usernames and initial passwords
+
+
+### admin action out of python
+
+# tbc
+
+
---
+++ new_course.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# admin script to install a new course into dh2go
+# expects to be run in a folder with 
+#	adduser.conf
+#  skel directory
+#	teiln.csv with minimum of one column containing full names in the form prename surname
+
+# output
+#	user.list: file with just a list of users suitable for use with console commands
+#	user:pass: file with a list of values in the form username:password for use with chpasswd
+# 	teiln_full.csv: like teiln.csv with three additional columns username, password, user:pass
+
+# variables - please fill out for every new course
+
+GROUP="dh2go_ws"
+TEACHER="vagrant"
+
+######################################
+
+# get base location 
+BASEDIR=$(pwd)
+
+# add course group
+addgroup $GROUP
+
+# add course folders
+mkdir /srv/$GROUP
+mkdir /srv/$GROUP/Kursmaterial
+mkdir /srv/$GROUP/Austauschordner
+
+# add symbolic link to course folder to the skel folder
+ln -s /srv/$GROUP $BASEDIR/skel/Desktop/
+
+# add teacher account
+adduser --conf adduser.conf --geco "" --disabled-login $line
+
+# change rights for course folders
+chown -R $TEACHER:$GROUP /srv/$GROUP
+chmod 750 /srv/$GROUP
+chmod 770 /srv/$GROUP/Austauschordner
+
+# build userlist and user:pass list
+python3 new_course.py
+
+
+# add users 
+while read line
+do
+	echo $line
+	adduser --conf adduser.conf --geco "" --disabled-login $line
+done < user.list
+
+
+# set all users passwords according to user.pass
+while read line
+	do 
+		chpasswd $line
+	done < user.pass
+
+
